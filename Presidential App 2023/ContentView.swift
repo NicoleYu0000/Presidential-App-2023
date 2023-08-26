f5e3b67911fb453bf933732b3329690b9c3c9bd3
NicoleYu0000 <littleco6688@gmail.com>
2023-08-26 10:29:58 +0800

    Initial Commit

---
+++ Presidential App 2023/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Presidential App 2023
+//
+//  Created by Nicole Yu on 2023/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Presidential App 2023/Presidential_App_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Presidential_App_2023App.swift
+//  Presidential App 2023
+//
+//  Created by Nicole Yu on 2023/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Presidential_App_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
